e51d3c251996ca70caf6ce84ef34ac914588faa7
bonbonpa <sphinspf@gmail.com>
2018-04-09 07:22:44 +0700

    update : js function

--- 04.Function/ch1.js
+++ 04.Function/ch1.js
@@ -19,3 +19,19 @@ printSomething();
 console.log(add(1, 2));
 let sum = add2(1, 2, 3);
 console.log(sum);
+
+/**
+ * Function #2: ES6 Feature - Declaration
+ */
+
+// ES6 : Declaration, Arrows are a function shorthand (=>)
+const add3 = (a, b) => {
+  console.log("a+b=", a + b);
+  return a + b;
+};
+
+const add1 = a => a + 1;
+
+console.log(add3(1, 2)); // 3
+console.log(add1(1)); // 2
+console.log([1, 2, 3].map(add1)); // [2,3,4]
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Learning Modern JavaScript
+
+Link Reference
+
+- [Modern JavaScript Programming language with ES6/ES2015](https://kobkrit.com/react-native-tutorial-lecture-2-modern-javascript-programming-language-with-es6-es2015-98eefadf6b0d)
